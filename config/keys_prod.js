4f083e8e6be90b5e9e305a5b75facade272239ff
Harshithpabbati <pabbatiharshith@gmail.com>
2019-04-20 14:08:15 +0530

    deploy

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://harshith:<password>@social-network-q9xsj.mongodb.net/test?retryWrites=true',
+  mongoURI: 'mongodb+srv://harshith:harsha2001@social-network-q9xsj.mongodb.net/test?retryWrites=true',
   secretOrKey: 'SECRET'
 };
